3c99a6637ecfc5413c8c0396af18a3f422370c0e
M4rakesh <maksims.rogozins@gmail.com>
2024-09-10 12:12:37 +0300

    Update asdjfh.py

--- asdjfh.py
+++ asdjfh.py
@@ -1,1 +1,9 @@
-print("hello")
+#print("hello")
+
+p=["Merkurs", "Venēra", "Zeme", "Marss", "Jupiters", "Saturns", "Urāns", "Neptūns"]
+
+a=len(p)
+print(f"Saules sistēmā ir {a} planētas.")
+
+for i, item in enumerate(p,1):
+    print(f"{item,i}.vieta")
